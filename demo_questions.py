018ebdbb2bad2f57bdf6818b87fe6e62318ac275
UjjawalRoy <royujjawal@gmail.com>
2024-04-19 00:25:17 +0530

    Added new features

---
+++ demo_questions.py
@@ -0,0 +1,3 @@
+demo_ques = ["What is artificial intelligence?",
+             "Where is AI technology used?",
+             "Which applications make use of advance AI?"]
--- frontend.py
+++ frontend.py
@@ -3,10 +3,12 @@ import json
 import streamlit as st
 import requests
 
+import demo_questions
+
 
 def create_database_page():
     st.title("Create Database")
-    url = st.text_input("Enter URL")
+    url = st.text_input("Enter Wiki URL")
     if st.button("Submit"):
         response = requests.post("http://localhost:8001/createdb", json={"url": url})
         if response.status_code == 200:
@@ -16,10 +18,22 @@ def create_database_page():
 
 
 def ask_question_page():
-    st.title("Ask Question")
+    st.title("Wikipedia Chatbot")
+    query = st.selectbox("Select a question", ["What is artificial intelligence?",
+                                               "Where is AI technology used?",
+                                               "Which applications make use of advance AI?",
+                                               "Who was the first person to conduct substantial research on AI?",
+                                               "What are knowledge representation and knowledge engineering?",
+                                               "What is a knowledge base?",
+                                               "What is a rational 'Agent'?"])
+    st.text('Or')
     question = st.text_input("Enter your question")
     if st.button("Ask"):
-        response = requests.post("http://localhost:8001/ask", data=json.dumps({"query": question.lower()}))
+        response = ''
+        if question:
+            response = requests.post("http://localhost:8001/ask", data=json.dumps({"query": question.lower()}))
+        else:
+            response = requests.post("http://localhost:8001/ask", data=json.dumps({"query": query.lower()}))
         if response.status_code == 200:
             st.success("Answer: " + response.json())
         else:
